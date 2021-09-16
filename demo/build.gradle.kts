3ae49946c6dceebe4c214d1773d507b98e3f8117
Robert <bigbob1991@gmail.com>
2021-09-16 18:34:04 +0400

    v2.1.4 update

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -36,9 +36,9 @@ dependencies {
   implementation("androidx.core:core-ktx:1.6.0")
   implementation("androidx.appcompat:appcompat:1.3.1")
   implementation("com.google.android.material:material:1.4.0")
-  implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+  implementation("androidx.constraintlayout:constraintlayout:2.1.0")
   implementation("io.coil-kt:coil:1.3.0")
   implementation("io.coil-kt:coil-video:1.1.1")
 //  implementation(project(mapOf("path" to ":lib")))
-  implementation("com.robertlevonyan.components:Picker:2.1.5")
+  implementation("com.robertlevonyan.components:Picker:2.1.6")
 }
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -46,8 +46,8 @@ allprojects {
 dependencies {
   implementation(kotlin("stdlib"))
   implementation("androidx.appcompat:appcompat:1.3.1")
-  implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+  implementation("androidx.constraintlayout:constraintlayout:2.1.0")
   implementation("com.google.android.material:material:1.4.0")
-  implementation("androidx.core:core-ktx:1.7.0-alpha01")
+  implementation("androidx.core:core-ktx:1.7.0-beta01")
   api("androidx.fragment:fragment-ktx:1.3.6")
 }
